9d864dfeaed7f6edf447a25b67ee10d6bd92b60f
Kevin Kim <kevinfkim@gmail.com>
2020-02-05 16:16:38 +0000

    Arrays Quizes

---
+++ arrays/arraysquiz_6-3.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.5, 9.99, 1.0, 1.1, 67.0];
+
+// your code goes here
+
+prices[0] = "1";
+prices[2] = "3";
+prices[6] = "7";
+
+console.log(prices);
