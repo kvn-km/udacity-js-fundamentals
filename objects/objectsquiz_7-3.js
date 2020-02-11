1acc138920ac03c9a9e9ec924f586b757b07857c
Kevin Kim <kevinfkim@gmail.com>
2020-02-11 02:56:00 +0000

    Objects Quizes

---
+++ objects/objectsquiz_7-3.js
@@ -0,0 +1,18 @@
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  },
+  printAccountSummary: function() {
+    return "Welcome!\nYour balance is currently $1000 and your interest rate is 1%.";
+  }
+};
